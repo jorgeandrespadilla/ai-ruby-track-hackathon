44e1a34587c854224e381e28012b62df70038f7e
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2024-08-17 18:04:41 -0500

    feat: add Drizzle ORM files and migrations

---
+++ db/schema.ts
@@ -0,0 +1,13 @@
+import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+
+export const usersTable = pgTable('users_table', {
+  id: serial('id').primaryKey(),
+  clerkId: text('clerk_id').notNull().unique(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at')
+    .notNull()
+    .$onUpdate(() => new Date()),
+});
+
+export type InsertUser = typeof usersTable.$inferInsert;
+export type SelectUser = typeof usersTable.$inferSelect;
