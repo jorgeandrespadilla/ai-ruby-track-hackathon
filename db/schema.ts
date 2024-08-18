55474ef88d09aac24eba600daffade12cdaa2c41
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2024-08-18 13:19:57 -0500

    feat: update DB schema to include complaints and transripts

--- db/schema.ts
+++ db/schema.ts
@@ -1,13 +1,33 @@
-import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
 
 export const usersTable = pgTable('users_table', {
   id: serial('id').primaryKey(),
   clerkId: text('clerk_id').notNull().unique(),
   createdAt: timestamp('created_at').notNull().defaultNow(),
-  updatedAt: timestamp('updated_at')
+});
+
+export const transcriptsTable = pgTable('transcripts_table', {
+  id: serial('id').primaryKey(),
+  userId: integer('user_id')
     .notNull()
-    .$onUpdate(() => new Date()),
+    .references(() => usersTable.id, { onDelete: 'cascade' }),
+  fileUrl: text('file_url'),
+  text: text('text'),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+});
+
+export const complaintsTable = pgTable('complaints_table', {
+  id: serial('id').primaryKey(),
+  transcriptId: integer('transcript_id')
+    .notNull()
+    .references(() => transcriptsTable.id, { onDelete: 'cascade' }),
+  summary: text('summary').notNull().default(''),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
 });
 
 export type InsertUser = typeof usersTable.$inferInsert;
 export type SelectUser = typeof usersTable.$inferSelect;
+export type InsertTranscript = typeof transcriptsTable.$inferInsert;
+export type SelectTranscript = typeof transcriptsTable.$inferSelect; 
+export type InsertComplaint = typeof complaintsTable.$inferInsert;
+export type SelectComplaint = typeof complaintsTable.$inferSelect;
