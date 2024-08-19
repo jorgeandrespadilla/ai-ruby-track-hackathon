65d4b0b3394b18acc5ade8cc4fa0894dff73568f
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2024-08-18 20:07:26 -0500

    feat: update API route for saving data

--- app/api/savedata/route.ts
+++ app/api/savedata/route.ts
@@ -1,13 +1,20 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from 'next/server';
 import { saveTranscriptAndComplaints } from '@/db/queries';
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-  const { userId, transcript, fileUrl, analysis } = req.body;
+interface TranscriptAndComplaints {
+  userId: string;
+  transcript: string;
+  fileUrl: string;
+  analysis: string;
+}
+
+export async function POST(req: NextRequest) {
+  const { userId, transcript, fileUrl, analysis }: TranscriptAndComplaints = await req.json();
 
   try {
     const body = await saveTranscriptAndComplaints({ userId, transcript, fileUrl, analysis });
-    return Response.json({"body": body });
+    return NextResponse.json({"body": body });
   } catch (error) {
-    
+    return NextResponse.json({"error": error }, { status: 500 });
   }
 }
